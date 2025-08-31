7d7fa6c96e77517dc3077f11021965669ff2c61b
prajapatkavitha <143082071+prajapatkavitha@users.noreply.github.com>
2025-08-31 17:05:28 +0530

    Update urls.py

--- account/urls.py
+++ account/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import *
+from .views import UserRegistrationView, CustomTokenObtainPairView
 
 urlpatterns = [
-    
+    path('register/', UserRegistrationView.as_view(), name='user-register'),
+    path('login/', CustomTokenObtainPairView.as_view(), name='token-obtain-pair'),
 ]
