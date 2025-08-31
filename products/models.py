508e7cc52e2eb537509d7eb04bf90f35be15d41b
prajapatkavitha <143082071+prajapatkavitha@users.noreply.github.com>
2025-08-31 16:44:24 +0530

    Update models.py

--- products/models.py
+++ products/models.py
@@ -1,8 +1,5 @@
 from django.db import models
 
-# Create your models here.
-from django.db import models
-
 class Category(models.Model):
     name = models.CharField(max_length=100)
 
@@ -17,6 +14,7 @@ class Menu(models.Model):
 
     def __str__(self):
         return self.name
+
 class Item(models.Model):
     item_name = models.CharField(max_length=150)
     item_price = models.DecimalField(max_digits=10, decimal_places=2)
