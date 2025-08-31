bf4e460b1f936878ce0d2c8652dde522d4164bda
prajapatkavitha <143082071+prajapatkavitha@users.noreply.github.com>
2025-08-31 17:05:01 +0530

    Create serializers.py

---
+++ account/serializers.py
@@ -0,0 +1,22 @@
+from django.contrib.auth import get_user_model
+from rest_framework import serializers
+from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
+
+User = get_user_model()
+
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ['username', 'password', 'role']
+        extra_kwargs = {'password': {'write_only': True}}
+
+    def create(self, validated_data):
+        user = User.objects.create_user(**validated_data)
+        return user
+
+class CustomTokenObtainPairSerializer(TokenObtainPairSerializer):
+    def validate(self, attrs):
+        data = super().validate(attrs)
+        # Add user role to the token payload
+        data['role'] = self.user.role
+        return data
