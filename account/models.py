e82a7360c0d1f61eb606f93eb727cefd4d608b02
prajapatkavitha <143082071+prajapatkavitha@users.noreply.github.com>
2025-08-31 15:27:36 +0530

    Update models.py

--- account/models.py
+++ account/models.py
@@ -1,3 +1,21 @@
-from django.db import models
+# account/models.py
 
-# Create your models here.
+from django.db import models
+from django.contrib.auth.models import AbstractUser
+
+class User(AbstractUser):
+    # Choices for user roles
+    class Role(models.TextChoices):
+        ADMIN = "Admin", "Admin"
+        MANAGER = "Manager", "Manager"
+        CASHIER = "Cashier", "Cashier"
+        WAITER = "Waiter", "Waiter"
+
+    role = models.CharField(
+        max_length=10,
+        choices=Role.choices,
+        default=Role.WAITER
+    )
+
+    def __str__(self):
+        return f"{self.username} ({self.role})"
