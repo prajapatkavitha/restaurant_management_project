8a0621ba7a564fbc689f4cd06986ecd5eeb9919a
prajapatkavitha <143082071+prajapatkavitha@users.noreply.github.com>
2025-08-31 17:04:21 +0530

    Update views.py

--- account/views.py
+++ account/views.py
@@ -1,3 +1,15 @@
-from django.shortcuts import render
+from django.contrib.auth import get_user_model
+from rest_framework import generics, permissions
+from rest_framework.response import Response
+from rest_framework_simplejwt.views import TokenObtainPairView
+from .serializers import UserSerializer, CustomTokenObtainPairSerializer
 
-# Create your views here.
+User = get_user_model()
+
+class UserRegistrationView(generics.CreateAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+    permission_classes = [permissions.AllowAny]
+
+class CustomTokenObtainPairView(TokenObtainPairView):
+    serializer_class = CustomTokenObtainPairSerializer
