8b33a1bf55ba27cbaf4eae4379a8dde5bbaa4cf0
prajapatkavitha <143082071+prajapatkavitha@users.noreply.github.com>
2025-09-07 11:30:13 +0530

    Update urls.py

--- orders/urls.py
+++ orders/urls.py
@@ -1,11 +1,12 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from .views import OrderView, WaiterOrderViewSet
+from .views import OrderView, WaiterOrderViewSet, TopCustomersReportView
 
 router = DefaultRouter()
 router.register(r'waiter-orders', WaiterOrderViewSet, basename='waiter-orders')
 
 urlpatterns = [
     path('orders/', OrderView.as_view(), name='orders-list'),
+    path('reports/top-customers/', TopCustomersReportView.as_view(), name='top-customers-report'),
     path('', include(router.urls)),
 ]
