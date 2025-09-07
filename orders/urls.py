14ffcf5a95b182271d9e62290c2f023058e96c74
prajapatkavitha <143082071+prajapatkavitha@users.noreply.github.com>
2025-09-07 11:27:43 +0530

    Update urls.py

--- orders/urls.py
+++ orders/urls.py
@@ -1,6 +1,11 @@
-from django.urls import path
-from .views import *
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
+from .views import OrderView, WaiterOrderViewSet
+
+router = DefaultRouter()
+router.register(r'waiter-orders', WaiterOrderViewSet, basename='waiter-orders')
 
 urlpatterns = [
-    
+    path('orders/', OrderView.as_view(), name='orders-list'),
+    path('', include(router.urls)),
 ]
