9cc2d69fd0b9d7ed094812ed20b252c3f7f4c4fc
prajapatkavitha <143082071+prajapatkavitha@users.noreply.github.com>
2025-09-07 10:47:19 +0530

    Update views.py

--- products/views.py
+++ products/views.py
@@ -2,32 +2,15 @@ from django.shortcuts import render
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
-from rest_framework.permissions import IsAuthenticated
-
-from account.permissions import IsManagerOrAdmin
-from .models import Item
-from .serializers import ItemSerializer
-
 from rest_framework import viewsets
-from .models import Menu
-from .serializers import MenuSerializer
-from rest_framework.permissions import IsAuthenticated
+from rest_framework.permissions import IsAuthenticated, AllowAny
+from django.core.cache import cache
+
+from .models import Item, Menu
+from .serializers import ItemSerializer, MenuSerializer
 from account.permissions import IsManagerOrAdmin
 
-class MenuViewSet(viewsets.ModelViewSet):
-    queryset = Menu.objects.all().select_related('category')
-    serializer_class = MenuSerializer
-    permission_classes = [IsAuthenticated, IsManagerOrAdmin]
-'''
-NOTE: Consider this as a reference and follow this same coding structure or format to work on your tasks
-'''
-
-# Create your views here.
 class ItemView(APIView):
-    # This line applies the permissions to both GET and POST requests.
-    # Users must be authenticated, and their role must be Admin or Manager.
-    permission_classes = [IsAuthenticated, IsManagerOrAdmin]
-
     def get(self, request):
         items = Item.objects.all()
         serializer = ItemSerializer(items, many=True)
@@ -40,4 +23,25 @@ class ItemView(APIView):
             return Response(serializer.data, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
+class PopularMenuView(APIView):
+    permission_classes = [AllowAny]
+    def get(self, request, *args, **kwargs):
+        cache_key = 'popular_menu_items'
+        
+        # Try to get data from the cache
+        items = cache.get(cache_key)
 
+        if not items:
+            # If not in cache, fetch from the database
+            items = Menu.objects.order_by('-order_count')[:5] 
+            
+            # Cache the data for 5 minutes (300 seconds)
+            cache.set(cache_key, items, 300)
+            
+        serializer = MenuSerializer(items, many=True)
+        return Response(serializer.data)
+
+class MenuViewSet(viewsets.ModelViewSet):
+    queryset = Menu.objects.all().select_related('category')
+    serializer_class = MenuSerializer
+    permission_classes = [IsAuthenticated, IsManagerOrAdmin]
