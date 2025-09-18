06c20ab17346b468e0c0df5d09fe457bda727bae
prajapatkavitha <143082071+prajapatkavitha@users.noreply.github.com>
2025-09-18 12:59:20 +0530

    Update urls.py

--- orders/urls.py
+++ orders/urls.py
@@ -1,12 +1,24 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from .views import OrderView, WaiterOrderViewSet, TopCustomersReportView
+from .views import (
+    OrderView,
+    WaiterOrderViewSet,
+    ReservationView,
+    TopCustomersReportView,
+    CouponViewSet,
+)
 
+# Create a router instance to handle ViewSets
 router = DefaultRouter()
-router.register(r'waiter-orders', WaiterOrderViewSet, basename='waiter-orders')
+router.register(r'waiter-orders', WaiterOrderViewSet, basename='waiter-order')
+router.register(r'coupons', CouponViewSet, basename='coupon')
 
 urlpatterns = [
-    path('orders/', OrderView.as_view(), name='orders-list'),
-    path('reports/top-customers/', TopCustomersReportView.as_view(), name='top-customers-report'),
+    # Router URLs for the ViewSets
     path('', include(router.urls)),
+
+    # URLs for the specific APIView and generics views
+    path('orders/', OrderView.as_view(), name='order-list-create'),
+    path('reservations/', ReservationView.as_view(), name='reservation-create'),
+    path('reports/top-customers/', TopCustomersReportView.as_view(), name='top-customers-report'),
 ]
