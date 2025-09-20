8364038d332b677f97baf04a8a0d251509621d06
prajapatkavitha <143082071+prajapatkavitha@users.noreply.github.com>
2025-09-20 15:00:28 +0530

    Update serializers.py

--- account/serializers.py
+++ account/serializers.py
@@ -1,14 +1,21 @@
 from django.contrib.auth import get_user_model
 from rest_framework import serializers
 from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
+from .utils import is_valid_email # Import the new utility function
 
 User = get_user_model()
 
 class UserSerializer(serializers.ModelSerializer):
+    email = serializers.EmailField(required=True)
     class Meta:
         model = User
-        fields = ['username', 'password', 'role']
+        fields = ['username', 'password', 'email', 'role']
         extra_kwargs = {'password': {'write_only': True}}
+
+    def validate_email(self, value):
+        if not is_valid_email(value):
+            raise serializers.ValidationError("Please enter a valid email address.")
+        return value
 
     def create(self, validated_data):
         user = User.objects.create_user(**validated_data)
