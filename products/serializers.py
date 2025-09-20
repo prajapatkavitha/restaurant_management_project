63bbfb15ead9b8617e8c8209a9cee7a491200f34
prajapatkavitha <143082071+prajapatkavitha@users.noreply.github.com>
2025-09-20 14:27:15 +0530

    Update serializers.py

--- products/serializers.py
+++ products/serializers.py
@@ -1,12 +1,20 @@
 from rest_framework import serializers
-from .models import Item
+from .models import Menu, Category
 
 class ItemSerializer(serializers.ModelSerializer):
+    """
+    Serializer for the Item model.
+    """
     class Meta:
         model = Item
         fields = '__all__'
+
 class MenuSerializer(serializers.ModelSerializer):
+    """
+    Serializer for the Menu model, including all relevant fields.
+    """
     category = serializers.CharField(source='category.name', read_only=True)
+    
     class Meta:
         model = Menu
-        fields = ['id', 'name', 'price', 'category']
+        fields = ['id', 'name', 'description', 'price', 'category']
