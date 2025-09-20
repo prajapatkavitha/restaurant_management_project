3e8ad12fbf82337edf53f9971df8af73ebe60f65
prajapatkavitha <143082071+prajapatkavitha@users.noreply.github.com>
2025-09-20 14:29:41 +0530

    Update urls.py

--- products/urls.py
+++ products/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from .views import PopularDishesReportView, PopularMenuView, MenuViewSet
+from .views import PopularDishesReportView, PopularMenuView, MenuViewSet, MenuCategoryListView
 
 router = DefaultRouter()
 router.register(r'', MenuViewSet, basename='menu')
@@ -8,5 +8,6 @@ router.register(r'', MenuViewSet, basename='menu')
 urlpatterns = [
     path('popular/', PopularMenuView.as_view(), name='popular-menu'),
     path('reports/popular-dishes/', PopularDishesReportView.as_view(), name='popular-dishes-report'),
+    path('by-category/', MenuCategoryListView.as_view(), name='menu-by-category'),
     path('', include(router.urls)),
 ]
