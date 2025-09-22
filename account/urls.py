a5e5342d3c316de2d7c0cc9506ce0a281a09338f
prajapatkavitha <143082071+prajapatkavitha@users.noreply.github.com>
2025-09-22 14:11:15 +0530

    Update urls.py

--- account/urls.py
+++ account/urls.py
@@ -1,7 +1,12 @@
-from django.urls import path
-from .views import UserRegistrationView, CustomTokenObtainPairView
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
+from .views import UserRegistrationView, CustomTokenObtainPairView, UserUpdateViewSet
+
+router = DefaultRouter()
+router.register(r'profile', UserUpdateViewSet, basename='user-profile')
 
 urlpatterns = [
     path('register/', UserRegistrationView.as_view(), name='user-register'),
     path('login/', CustomTokenObtainPairView.as_view(), name='token-obtain-pair'),
+    path('', include(router.urls)),
 ]
