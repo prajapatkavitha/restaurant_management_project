512fcb0184107eb3c0185b095bd606eee47fee9c
prajapatkavitha <143082071+prajapatkavitha@users.noreply.github.com>
2025-09-22 14:10:07 +0530

    Update views.py

--- account/views.py
+++ account/views.py
@@ -1,8 +1,10 @@
-from django.contrib.auth import get_user_model
-from rest_framework import generics, permissions
+from rest_framework import viewsets, permissions, status
 from rest_framework.response import Response
+from rest_framework import mixins
+from django.contrib.auth import get_user_model
+from .serializers import UserUpdateSerializer
 from rest_framework_simplejwt.views import TokenObtainPairView
-from .serializers import UserSerializer, CustomTokenObtainPairSerializer
+from rest_framework import generics
 
 User = get_user_model()
 
@@ -13,3 +15,23 @@ class UserRegistrationView(generics.CreateAPIView):
 
 class CustomTokenObtainPairView(TokenObtainPairView):
     serializer_class = CustomTokenObtainPairSerializer
+
+class UserUpdateViewSet(
+    mixins.RetrieveModelMixin,
+    mixins.UpdateModelMixin,
+    viewsets.GenericViewSet
+):
+    """
+    A ViewSet to allow a user to retrieve and update their own profile.
+    Only allows GET and PUT/PATCH methods.
+    """
+    queryset = User.objects.all()
+    serializer_class = UserUpdateSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+    def get_object(self):
+        """
+        Returns the object the view is displaying. In this case, it's always the
+        logged-in user's profile.
+        """
+        return self.request.user
