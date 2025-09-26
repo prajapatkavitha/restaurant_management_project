8d87ff1c11d37e6ff504889052b7430a472edfce
prajapatkavitha <143082071+prajapatkavitha@users.noreply.github.com>
2025-09-26 14:00:27 +0530

    Update urls.py

--- home/urls.py
+++ home/urls.py
@@ -1,6 +1,13 @@
 from django.urls import path
 from .views import CategoryListAPIView
+from django.contrib import admin
+from django.urls import path, include
+
 
 urlpatterns = [
     path('categories/', CategoryListAPIView.as_view(), name='category-list'),
+    path('admin/', admin.site.urls),
+    path('api/contact/', include('contact.urls')),
+
 ]
+
