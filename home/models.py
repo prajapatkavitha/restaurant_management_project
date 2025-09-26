7404c2875e5c3878d10c55e8676f5316f4f5cca6
prajapatkavitha <143082071+prajapatkavitha@users.noreply.github.com>
2025-09-26 13:51:37 +0530

    Update models.py

--- home/models.py
+++ home/models.py
@@ -9,7 +9,20 @@ class MenuCategory(models.Model):
 
     def __str__(self):
         return self.name
-        
-python manage.py makemigrations home
-python manage.py migrate
 
+class ContactFormSubmission(models.Model):
+    """
+    Model to store contact form submissions from the contact form API.
+    """
+    name = models.CharField(max_length=255)
+    email = models.EmailField()
+    message = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        # Makes the model appear as "Contact Form Submissions" in the Django admin
+        verbose_name_plural = "Contact Form Submissions"
+
+    def __str__(self):
+        # Human-readable representation of the object
+        return self.name
